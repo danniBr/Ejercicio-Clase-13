78d164ecccb4f3792b9b089de1210c083eee2601
danniBr <danitza.br@gmail.com>
2018-06-17 18:52:20 -0400

    commit ejercicio 2 modificado

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,4 +1,26 @@
 productos = { 'bebida' => 850, 'chocolate' => 1200,
               'galletas' => 900, 'leche' => 750 }
 
+# 1:
 productos.each { |producto, valor| puts producto }
+
+# 2:
+productos['cereal'] = 2200
+
+puts productos
+
+# 3:
+productos['bebida'] = 2000
+
+puts productos
+
+# 4:
+
+productos_array = productos.to_a
+
+print productos_array
+
+# 5:
+productos.delete('galletas')
+
+puts productos
